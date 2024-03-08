f3d0d769899c2cf669df7ee5a8ef7840e20a2982
Mereke Sabitov <merekesabitov@gmail.com>
2024-03-08 20:48:34 +0500

    Weather console app

--- weather_console_app/bin/main.dart
+++ weather_console_app/bin/main.dart
@@ -1,9 +1,9 @@
-
+import 'dart:io';
 
 import 'package:weather_console_app/api_service.dart';
 
 void main(List<String> arguments) async {
-  if(arguments.length != 1) {
+  if (arguments.length != 1) {
     print('Syntax error:  dart bin/main.dart <city>');
     return;
   }
@@ -12,8 +12,18 @@ void main(List<String> arguments) async {
   //print(cityName);
 
   final weatherApiService = ApiService();
-  final currentWeather = await weatherApiService.getCurrentWeather(cityName);
-  print(currentWeather.cityName);
-  print(currentWeather.tempC);
 
+//Getting current weather
+  try {
+    final currentWeather = await weatherApiService.getCurrentWeather(cityName);
+    print('Current weather for $cityName');
+    print(currentWeather);
+    print('-----------------------\n');
+  } on WeatherApiException catch (error) {
+    print('Error ${error.message}');
+  } on SocketException catch (_) {
+    print('Сould not fetch current weather. Check yout internet');
+  } on Exception catch (e) {
+    print('$e');
+  }
 }
